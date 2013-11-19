cd67e1fd56ad65f994ea59d890d6ffa59d238e4a
Duy. Le Ngoc <duyln@vng.com.vn>
2013-11-19 10:25:58 +0700

    Initial Commit

---
+++ ADN/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ADN
+//
+//  Created by Le Ngoc Duy on 11/19/13.
+//  Copyright (c) 2013 Le Ngoc Duy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
